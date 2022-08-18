ed90a7250213b2afc3615ee6693ab42c748d27d4
Sunshine <sunshine@uberspace.net>
2022-08-18 18:07:32 -0400

    add ability to parse registrar info

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 pub struct DomainProps {
     pub domain_name: String,
     pub expiration_date: String,
+    pub registrar: Option<String>,
     pub is_registered: bool,
     pub is_under_grace_period: bool,
 }
@@ -12,36 +13,37 @@ pub fn parse_info(domain_name: &str, whois_info: &str) -> DomainProps {
     let mut whois_data = DomainProps {
         domain_name: String::from(domain_name),
         expiration_date: String::new(),
+        registrar: None,
         is_registered: false,
         is_under_grace_period: false,
     };
 
     let lines = whois_info.lines();
     for line in lines {
-        let line_trimmed = line.trim();
+        let line = line.trim();
 
         // Determine if domain is registered
-        if line_trimmed.starts_with("Domain not found.")
-            || line_trimmed.starts_with("Domain not registered.")
-            || line_trimmed.starts_with("No match for")
-            || line_trimmed.starts_with("% No entries found for query")
+        if line.starts_with("Domain not found.")
+            || line.starts_with("Domain not registered.")
+            || line.starts_with("No match for")
+            || line.starts_with("% No entries found for query")
         {
             break;
         }
 
         // Parse expiration date
-        if line_trimmed.starts_with("Registry Expiry Date:") {
+        if line.starts_with("Registry Expiry Date:") {
             let re = Regex::new(r"Registry Expiry Date:\s+(.*)").unwrap();
-            for caps in re.captures_iter(line_trimmed) {
+            for caps in re.captures_iter(line) {
                 let result = caps.get(1).unwrap().as_str();
                 let datetime_utc = result.parse::<DateTime<Utc>>().unwrap();
                 whois_data.is_registered = true;
                 whois_data.expiration_date = format!("{:?}", datetime_utc);
             }
             continue;
-        } else if line_trimmed.starts_with("Expiry date:") {
+        } else if line.starts_with("Expiry date:") {
             let re = Regex::new(r"Expiry date:\s+(.*)").unwrap();
-            for caps in re.captures_iter(line_trimmed) {
+            for caps in re.captures_iter(line) {
                 let naive_date =
                     NaiveDate::parse_from_str(caps.get(1).unwrap().as_str(), "%d-%B-%Y").unwrap();
                 let naive_datetime: NaiveDateTime = naive_date.and_hms(0, 0, 0);
@@ -50,9 +52,9 @@ pub fn parse_info(domain_name: &str, whois_info: &str) -> DomainProps {
                 whois_data.expiration_date = format!("{:?}", datetime_utc);
             }
             continue;
-        } else if line_trimmed.starts_with("expires:") {
+        } else if line.starts_with("expires:") {
             let re = Regex::new(r"expires:\s+(.*)").unwrap();
-            for caps in re.captures_iter(line_trimmed) {
+            for caps in re.captures_iter(line) {
                 let naive_date =
                     NaiveDate::parse_from_str(caps.get(1).unwrap().as_str(), "%B %d %Y").unwrap();
                 let naive_datetime: NaiveDateTime = naive_date.and_hms(0, 0, 0);
@@ -61,9 +63,9 @@ pub fn parse_info(domain_name: &str, whois_info: &str) -> DomainProps {
                 whois_data.expiration_date = format!("{:?}", datetime_utc);
             }
             continue;
-        } else if line_trimmed.starts_with("Expiration date:") {
+        } else if line.starts_with("Expiration date:") {
             let re = Regex::new(r"Expiration date:\s+(.*)").unwrap();
-            for caps in re.captures_iter(line_trimmed) {
+            for caps in re.captures_iter(line) {
                 let naive_datetime = NaiveDateTime::parse_from_str(
                     caps.get(1).unwrap().as_str(),
                     "%d.%m.%Y %H:%M:%S",
@@ -74,12 +76,21 @@ pub fn parse_info(domain_name: &str, whois_info: &str) -> DomainProps {
                 whois_data.expiration_date = format!("{:?}", datetime_utc);
             }
             continue;
+        } else if line.starts_with("paid-till:") {
+            let re = Regex::new(r"paid-till:\s+(.*)").unwrap();
+            for caps in re.captures_iter(line) {
+                let result = caps.get(1).unwrap().as_str();
+                let datetime_utc = result.parse::<DateTime<Utc>>().unwrap();
+                whois_data.is_registered = true;
+                whois_data.expiration_date = format!("{:?}", datetime_utc);
+            }
+            continue;
         }
 
         // Parse status
-        if line_trimmed.starts_with("Domain Status:") {
+        if line.starts_with("Domain Status:") {
             let re = Regex::new(r"Domain Status:\s+(.*)").unwrap();
-            for caps in re.captures_iter(line_trimmed) {
+            for caps in re.captures_iter(line) {
                 let result = caps.get(1).unwrap().as_str();
                 if result == "redemptionPeriod https://icann.org/epp#redemptionPeriod" {
                     whois_data.is_under_grace_period = true;
@@ -87,9 +98,18 @@ pub fn parse_info(domain_name: &str, whois_info: &str) -> DomainProps {
             }
             continue;
         }
+
+        // Parse registrar name
+        if line.starts_with("Registrar:") || line.starts_with("registrar:") {
+            let re = Regex::new(r"(?i)Registrar:\s+(.*)").unwrap();
+            for caps in re.captures_iter(line) {
+                let result = caps.get(1).unwrap().as_str();
+                whois_data.registrar = Some(result.to_string());
+            }
+            continue;
+        }
     }
 
-    // TODO: parse more outputs (e.g. registrar)
     // TODO: throw errors instead of simply returning a struct
 
     return whois_data;
--- tests/parse_info.rs
+++ tests/parse_info.rs
@@ -21,6 +21,7 @@ mod passing {
 
         assert_eq!(domain_props.domain_name, "somesite.co.uk");
         assert_eq!(domain_props.expiration_date, "2022-05-14T00:00:00Z");
+        assert_eq!(domain_props.registrar, None);
         assert_eq!(domain_props.is_registered, true);
         assert_eq!(domain_props.is_under_grace_period, false);
     }
@@ -36,6 +37,7 @@ mod passing {
 
         assert_eq!(domain_props.domain_name, "crates.io");
         assert_eq!(domain_props.expiration_date, "2023-01-22T08:28:29Z");
+        assert_eq!(domain_props.registrar, Some("Gandi SAS".to_string()));
         assert_eq!(domain_props.is_registered, true);
         assert_eq!(domain_props.is_under_grace_period, false);
     }
@@ -51,6 +53,10 @@ mod passing {
 
         assert_eq!(domain_props.domain_name, "registered.rs");
         assert_eq!(domain_props.expiration_date, "2023-03-24T19:25:04Z");
+        assert_eq!(
+            domain_props.registrar,
+            Some("NINET Company d.o.o.".to_string())
+        );
         assert_eq!(domain_props.is_registered, true);
         assert_eq!(domain_props.is_under_grace_period, false);
     }
@@ -66,6 +72,23 @@ mod passing {
 
         assert_eq!(domain_props.domain_name, "site.is");
         assert_eq!(domain_props.expiration_date, "2021-03-14T00:00:00Z");
+        assert_eq!(domain_props.registrar, None);
+        assert_eq!(domain_props.is_registered, true);
+        assert_eq!(domain_props.is_under_grace_period, false);
+    }
+
+    #[test]
+    fn yandex_ru() {
+        let domain_name = "yandex.ru";
+        let whois_response_file_path_string: String = format!("tests/_data_/{}.txt", &domain_name);
+        let whois_response_file_path: &Path = Path::new(&whois_response_file_path_string);
+        let whois_response: String = fs::read_to_string(whois_response_file_path.as_os_str())
+            .expect("Something went wrong reading the file");
+        let domain_props = whoisthere::parse_info(domain_name, &whois_response);
+
+        assert_eq!(domain_props.domain_name, "yandex.ru");
+        assert_eq!(domain_props.expiration_date, "2022-09-30T21:00:00Z");
+        assert_eq!(domain_props.registrar, Some("RU-CENTER-RU".to_string()));
         assert_eq!(domain_props.is_registered, true);
         assert_eq!(domain_props.is_under_grace_period, false);
     }
