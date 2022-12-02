b444be5276ae00a630b37c4aaa379f48bff6bdca
Sunshine <sunshine@uberspace.net>
2022-12-02 05:33:52 -1000

    use enum instead of struct for status

--- src/parsers/status.rs
+++ src/parsers/status.rs
@@ -1,12 +1,9 @@
 use regex::Regex;
 
-use crate::{DomainPropStatus, DomainPropStatusFlag};
+use crate::DomainPropStatus;
 
 pub fn parse_status(whois_info: &str) -> DomainPropStatus {
-    let mut status = DomainPropStatus {
-        flag: DomainPropStatusFlag::Unknown,
-        url: None,
-    };
+    let mut status: DomainPropStatus = DomainPropStatus::Unknown;
 
     let lines = whois_info.lines();
 
@@ -20,7 +17,7 @@ pub fn parse_status(whois_info: &str) -> DomainPropStatus {
             || line_trimmed.starts_with("% No entries found for query")
         {
             // Nothing to do, get out of the loop
-            status.flag = DomainPropStatusFlag::Unregistered;
+            status = DomainPropStatus::Unregistered;
         }
     }
 
@@ -35,9 +32,7 @@ pub fn parse_status(whois_info: &str) -> DomainPropStatus {
             for caps in re.captures_iter(line) {
                 let result = caps.get(1).unwrap().as_str();
                 if result == "redemptionPeriod https://icann.org/epp#redemptionPeriod" {
-                    status.flag = DomainPropStatusFlag::GracePeriod;
-                    // TODO XXX FIXME
-                    status.url = Some("https://icann.org/epp#redemptionPeriod");
+                    status = DomainPropStatus::GracePeriod;
                 }
             }
             continue;
