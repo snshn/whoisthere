93d7daa8d57d2a210781369fa9f50bec68f539b5
Sunshine <sunshine@uberspace.net>
2022-12-02 22:45:21 -0500

    implement parsing of domain name for .uk domains

--- src/registrars/nominet.rs
+++ src/registrars/nominet.rs
@@ -5,6 +5,17 @@ use regex::Regex;
 
 use crate::DomainProps;
 
+#[derive(PartialEq)]
+enum Group {
+    No,
+    DomainName,
+    DataValidation,
+    Registrar,
+    RelevantDates,
+    RegistrationStatus,
+    NameServers,
+}
+
 pub fn parse_nominet_registrar_domain_whois_info<'a>(whois_info: &'a str) -> DomainProps<'a> {
     let mut domain_props = DomainProps {
         domain_name: "",
@@ -14,24 +25,60 @@ pub fn parse_nominet_registrar_domain_whois_info<'a>(whois_info: &'a str) -> Dom
     };
 
     let lines = whois_info.lines();
+    let mut group: Group = Group::No;
 
     for line in lines {
+        if line == "" {
+            group = Group::No;
+            continue;
+        }
+
         if line == "    This domain name has not been registered." {
             domain_props.is_registered = Some(false);
             break;
         }
 
-        if line.starts_with("        Expiry date:  ") {
-            let re = Regex::new(r"\s+Expiry date:\s+(.*)").unwrap();
+        if line == "    Domain name:" {
+            group = Group::DomainName;
+            continue;
+        } else if line == "    Data validation:" {
+            group = Group::DataValidation;
+            continue;
+        } else if line == "    Registrar:" {
+            group = Group::Registrar;
+            continue;
+        } else if line == "    Relevant dates:" {
+            group = Group::RelevantDates;
+            continue;
+        } else if line == "    Registration status:" {
+            group = Group::RegistrationStatus;
+            continue;
+        } else if line == "    Name servers:" {
+            group = Group::NameServers;
+            continue;
+        }
+
+        if group == Group::DomainName {
+            // Parse domain name
+            let re = Regex::new(r"\s+(.*)").unwrap();
             for caps in re.captures_iter(line) {
-                let naive_date =
-                    NaiveDate::parse_from_str(caps.get(1).unwrap().as_str(), "%d-%B-%Y").unwrap();
-                let naive_datetime: NaiveDateTime = naive_date.and_hms(0, 0, 0);
-                let datetime_utc = DateTime::<Utc>::from_utc(naive_datetime, Utc);
-                domain_props.expiry_date = Some(datetime_utc.to_rfc3339());
-                domain_props.is_registered = Some(true);
+                domain_props.domain_name = caps.get(1).unwrap().as_str();
             }
             continue;
+        } else if group == Group::RelevantDates {
+            if line.starts_with("        Expiry date:  ") {
+                let re = Regex::new(r"\s+Expiry date:\s+(.*)").unwrap();
+                for caps in re.captures_iter(line) {
+                    let naive_date =
+                        NaiveDate::parse_from_str(caps.get(1).unwrap().as_str(), "%d-%B-%Y")
+                            .unwrap();
+                    let naive_datetime: NaiveDateTime = naive_date.and_hms(0, 0, 0);
+                    let datetime_utc = DateTime::<Utc>::from_utc(naive_datetime, Utc);
+                    domain_props.expiry_date = Some(datetime_utc.to_rfc3339());
+                    domain_props.is_registered = Some(true);
+                }
+                continue;
+            }
         }
     }
 
