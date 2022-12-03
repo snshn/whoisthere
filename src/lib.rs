bfe5e42cf891a6efc1fc8826bbf5ccbdc6fa26a6
Sunshine <sunshine@uberspace.net>
2022-12-02 23:05:37 -0500

    rename parsing functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-mod registrars;
+mod parsers;
 
-use crate::registrars::dotgov::parse_dotgov_registrar_domain_whois_info;
-use crate::registrars::icann::parse_icann_registrar_domain_whois_info;
-use crate::registrars::isnic::parse_isnic_registrar_domain_whois_info;
-use crate::registrars::isocil::parse_isocil_registrar_domain_whois_info;
-use crate::registrars::nicit::parse_nicit_registrar_domain_whois_info;
-use crate::registrars::nominet::parse_nominet_registrar_domain_whois_info;
-use crate::registrars::rnids::parse_rnids_registrar_domain_whois_info;
-use crate::registrars::tcinet::parse_tcinet_registrar_domain_whois_info;
+use crate::parsers::dotgov::parse_dotgov_domain_whois_info;
+use crate::parsers::icann::parse_icann_domain_whois_info;
+use crate::parsers::isnic::parse_isnic_domain_whois_info;
+use crate::parsers::isocil::parse_isocil_domain_whois_info;
+use crate::parsers::nicit::parse_nicit_domain_whois_info;
+use crate::parsers::nominet::parse_nominet_domain_whois_info;
+use crate::parsers::rnids::parse_rnids_domain_whois_info;
+use crate::parsers::tcinet::parse_tcinet_domain_whois_info;
 
 pub struct DomainProps<'t> {
     pub domain_name: &'t str,
@@ -18,62 +18,62 @@ pub struct DomainProps<'t> {
 
 pub fn parse_domain_whois_info<'t>(domain_name: &'t str, whois_info: &'t str) -> DomainProps<'t> {
     if domain_name.ends_with(".com") {
-        let mut domain_info = parse_icann_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_icann_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".gov") {
-        let mut domain_info = parse_dotgov_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_dotgov_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".il") {
-        let mut domain_info = parse_isocil_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_isocil_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".io") {
-        let mut domain_info = parse_icann_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_icann_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".is") {
-        let mut domain_info = parse_isnic_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_isnic_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".it") {
-        let mut domain_info = parse_nicit_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_nicit_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".rs") {
-        let mut domain_info = parse_rnids_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_rnids_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".ru") {
-        let mut domain_info = parse_tcinet_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_tcinet_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else if domain_name.ends_with(".uk") {
-        let mut domain_info = parse_nominet_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_nominet_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
         return domain_info;
     } else {
         // // Everything else should fall onto ICANN
-        let mut domain_info = parse_icann_registrar_domain_whois_info(whois_info);
+        let mut domain_info = parse_icann_domain_whois_info(whois_info);
         if domain_info.domain_name == "" {
             domain_info.domain_name = domain_name;
         }
